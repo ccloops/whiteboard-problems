7165108909f64d1724750d5e958a4402873f09bf
Catherine Looper <loopercat@gmail.com>
2018-03-10 18:23:40 -0800

    solved reverse string problem without using reverse

---
+++ math/reverse-string.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const reverseStr = (str) => {
+  let result = '';
+
+  for(let i = str.length - 1; i >= 0; i--) {
+    result += str[i];
+  }
+  console.log(result);
+  return result;
+};
+
+reverseStr('bird');
