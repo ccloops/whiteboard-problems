2f9e87a7e34ff3e886d51b54e85d340c94e7dc13
Catherine Looper <loopercat@gmail.com>
2018-03-14 20:34:30 -0700

    solved find nth-digit kata

--- math/find-nth-digit.js
+++ math/find-nth-digit.js
@@ -10,13 +10,26 @@
 
 let findDigit = function(num, nth){
   let numberToReturn = 0;
-  if(nth < 0) {
+  if(nth <= 0) {
     numberToReturn = -1;
   }
 
+  if(num < 0) {
+    num = num * -1;
+  }
+
+  let numberArray = num.toString().split('').reverse();
+
+  for(let i = 0; i < numberArray.length; i++) {
+    if((nth - 1) === i) {
+      numberToReturn = parseInt(numberArray[i]);
+    }
+  }
   return numberToReturn;
 
 };
 
 
-findDigit(5673, 4);     // returns 5
+// findDigit(5673, 4);     // returns 5
+
+findDigit(65, 0); // returns -1 
