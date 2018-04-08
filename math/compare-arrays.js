334555428743a5ab178784993b6afb96b4cdb989
Catherine Looper <loopercat@gmail.com>
2018-04-08 14:46:30 -0700

    added a compare arrays method using toString method

---
+++ math/compare-arrays.js
@@ -0,0 +1,26 @@
+'use strict';
+
+let arrayToMatch = ['cheese', 'cheese'];
+let pizzaArray = ['cheese', 'cheese'];
+
+// if(arrayToMatch.length === pizzaArray.length) {
+
+// for(let i = 0; i < arrayToMatch.length; i++) {
+//   if(pizzaArray[i] === arrayToMatch[i]) {
+//     console.log('matches');
+//     console.log(pizzaArray[i]);
+//     console.log(arrayToMatch[i]);
+//   }
+// }
+
+console.log(arrayToMatch.toString());
+console.log(pizzaArray.toString());
+
+  
+if(arrayToMatch.toString() == pizzaArray.toString()){
+  console.log('matches');
+
+}
+
+ 
+
