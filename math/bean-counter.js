770c19f6faf693aebaa1175a0f4cd18ee4cfa282
Catherine Looper <loopercat@gmail.com>
2018-09-25 16:24:00 -0700

    added recursive problems, and bean counter solutions

---
+++ math/bean-counter.js
@@ -0,0 +1,31 @@
+// function countBs(str) {
+//   let counter = 0;
+
+//   for (let i = 0; i < str.length; i++) {
+//     if (str.charAt(i) === 'B') {
+//       counter++;
+//     }
+//   }
+//   return counter;
+// }
+
+// console.log(countBs("BBC"));
+// // → 2
+
+
+function countChar(string, ch) {
+  var counted = 0;
+  for (var i = 0; i < string.length; i++)
+    if (string.charAt(i) == ch)
+      counted ++;
+  return counted;
+}
+
+function countBs(string) {
+  return countChar(string, "B");
+}
+
+console.log(countBs("BBC"));
+// → 2
+console.log(countChar("kakkerlak", "k"));
+// → 4
---
+++ math/find-min.js
@@ -0,0 +1,8 @@
+function min(num1, num2) {
+  return num1 > num2 ? num2 : num1;
+}
+
+console.log(min(0, 10));
+// → 0
+console.log(min(0, -10));
+// → -10
