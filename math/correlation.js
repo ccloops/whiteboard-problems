f64783a48f86c37a617173c2ea4a1b97b69ea3d7
Catherine Looper <loopercat@gmail.com>
2018-09-27 13:54:52 -0700

    adding correlation examples

---
+++ math/correlation.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var map = {};
+function storePhi(event, phi) {
+  map[event] = phi;
+}
+
+storePhi('pizza', 0.069);
+storePhi('touched tree', -0.081);
+console.log('pizza' in map);
+console.log(['touched tree']);
+
+for(var event in map)
+  console.log("The correlation for '" + event + "' is " + map[event]);
+  console.log(map);
