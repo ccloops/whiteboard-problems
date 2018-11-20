56695ed55172f80087ae61b16db9fdce22243cbe
Catherine Looper <loopercat@gmail.com>
2018-11-19 22:13:34 -0800

    added a prepend(value) method and corresponding tests

--- __tests__/linked-lists/linked-list.test.js
+++ __tests__/linked-lists/linked-list.test.js
@@ -5,10 +5,45 @@ const LinkedList = require('../../lib/data-structures/linked-lists/singly/linked
 describe('LinkedList class methods', () => {
   it('append(value) method should append a value to the head of an empty Linked List', () => {
     let list = new LinkedList();
-    list.append(8);
-    console.log(list);
+    list.append(1);
 
-    expect(list.head.value).toEqual(8);
+    expect(list.head.value).toEqual(1);
     expect(list.head.next).toBeNull();
+  });
+
+  it('append(value) method should append a value to a Linked List that is not empty', () => {
+    let list = new LinkedList();
+    list.append(1);
+    list.append(2);
+    list.append(3);
+    list.append(4);
+
+    expect(list.head.value).toEqual(1);
+    expect(list.head.next.value).toEqual(2);
+    expect(list.head.next.next.value).toEqual(3);
+    expect(list.head.next.next.next.value).toEqual(4);
+    expect(list.head.next.next.next.next).toBeNull();
+  });
+
+  it('prepend(value) method should prepend a value to the head of an empty Linked List', () => {
+    let list = new LinkedList();
+    list.prepend(1);
+
+    expect(list.head.value).toEqual(1);
+    expect(list.head.next).toBeNull();
+  });
+
+  it('prepend(value) method should prepend a value to a Linked List that is not empty', () => {
+    let list = new LinkedList();
+    list.prepend(1);
+    list.prepend(2);
+    list.prepend(3);
+    list.prepend(4);
+
+    expect(list.head.value).toEqual(4);
+    expect(list.head.next.value).toEqual(3);
+    expect(list.head.next.next.value).toEqual(2);
+    expect(list.head.next.next.next.value).toEqual(1);
+    expect(list.head.next.next.next.next).toBeNull();
   });
 });
