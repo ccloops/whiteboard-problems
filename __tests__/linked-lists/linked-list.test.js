4a8fc519f46ab2006942b701518516923e043a69
Catherine Looper <loopercat@gmail.com>
2018-11-19 22:08:21 -0800

    linked list append method test added

--- __tests__/linked-lists/linked-list.test.js
+++ __tests__/linked-lists/linked-list.test.js
@@ -6,6 +6,7 @@ describe('LinkedList class methods', () => {
   it('append(value) method should append a value to the head of an empty Linked List', () => {
     let list = new LinkedList();
     list.append(8);
+    console.log(list);
 
     expect(list.head.value).toEqual(8);
     expect(list.head.next).toBeNull();
