836552235b3d9c54b5b8249ddd7b479425ca0d6b
Catherine Looper <loopercat@gmail.com>
2018-11-19 21:55:02 -0800

    adding linkmed list tests

---
+++ __tests__/linked-lists/lib/node.test.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const Node = require('../../../lib/data-structures/linked-lists/singly/node.js');
+
+describe('Linked List Node Class', () => {
+
+  it('constructor()', () => {
+    let node = new Node(3);
+    expect(node.value).toEqual(3);
+    expect(node.next).toBeNull();
+  });
+
+});
---
+++ __tests__/linked-lists/linked-list.test.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const LinkedList = require('../../lib/data-structures/linked-lists/singly/linked-list.js');
+
+describe('LinkedList class methods', () => {
+  it('append(value) method should append a value to the head of an empty Linked List', () => {
+    let list = new LinkedList();
+    list.append(8);
+
+    expect(list.head.value).toEqual(8);
+    expect(list.head.next).toBeNull();
+  });
+});
